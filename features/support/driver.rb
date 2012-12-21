083fa1a8cb5affa1536c29413a999cebbf9f3ae9
Steve Tooke <steve@heavi.es>
2012-12-21 12:57:24 +0000

    First failing step for find a song
      - Configure the players
      - Hit the web
      - Next bring in a Rails Controller!

--- features/support/driver.rb
+++ features/support/driver.rb
@@ -1,3 +1,30 @@
 require 'wedding_jukebox/core'
 
 World(WeddingJukebox::TestDataBuilder)
+
+module WeddingJukebox
+  module Players
+    def catalogue
+      @catalogue ||= FakeCatalogue.new #WeddingJukebox.catalogue
+    end
+
+    def jukebox
+      @jukebox ||= Jukebox.new(catalogue, ui)
+    end
+
+    def ui
+      @ui ||= WebUIDriver.new(self)
+    end
+
+    class WebUIDriver < SimpleDelegator
+      def search(term)
+        visit '/'
+        fill_in "Search for your favourite song", with: term
+        click_button "Go"
+      end
+    end
+  end
+end
+World(WeddingJukebox::Players)
+
+require 'capybara/cucumber'
