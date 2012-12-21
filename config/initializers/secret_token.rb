efa3162e372f32f686bce01f7eb4dd8c4c1ccfb1
Steve Tooke <steve@heavi.es>
2012-12-21 12:40:55 +0000

    Generate a rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingJukeboxWebUi::Application.config.secret_token = 'c320012a3a6e00eb0760459884007539d1976b6ddca1284647410e5e320c44919a624de6ed41a7ba0474c7a453697a82e28ed4994a0682b7477ce4402c72297c'
